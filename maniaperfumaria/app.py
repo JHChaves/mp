ab9f7aba550d4956317947f85a631c2000d4d53b
root <root@vps44295.publiccloud.com.br>
2024-09-13 09:42:30 -0300

    Inicio de CRUD para usuários. Criar usuário.

--- maniaperfumaria/app.py
+++ maniaperfumaria/app.py
@@ -1,8 +1,28 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from maniaperfumaria.schemas.schemas import (
+    Message,
+    UserDB,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def ler():
     return {'message': 'Meu belíssimo mundo!!!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,29 @@ from fastapi.testclient import TestClient
 from maniaperfumaria.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
+def test_root_deve_retornar_ok_e_meu_belissimo_mundo():
     client = TestClient(app)  # Arrange
 
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Meu belíssimo mundo!!!'}  # Assert
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
