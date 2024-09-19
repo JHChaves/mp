72ae7f01e56b90e8e81c7ae4007642b0a2f66e98
JHVC <jhvc.hermes@yahoo.com.br>
2024-09-19 10:30:33 -0300

    Implementado os endpoints de tarefas.

--- maniaperfumaria/app.py
+++ maniaperfumaria/app.py
@@ -2,13 +2,14 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from maniaperfumaria.routers import auth, users
+from maniaperfumaria.routers import auth, todos, users
 from maniaperfumaria.schemas import Message
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
